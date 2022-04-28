7549b95afaddbd9b6f1cbe20bd7b298ac8369cae
LuisEduardoRodriguez <luiseduarddo89@gmail.com>
2022-04-28 02:23:26 -0500

    proyecto_final

---
+++ routing/src/assets/js/efecto/index.js
@@ -0,0 +1,93 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.hero_header',{
+        duration: 2000,
+        origin: 'bottom',
+        distance:'-200px',
+
+    });
+    
+    sr.reveal('.hero__title',{
+        duration: 4000,
+        origin: 'bottom',
+        distance:'-200px',
+
+    });
+    sr.reveal('.hero__paragraph',{
+        duration: 3000,
+        origin: 'bottom',
+        distance:'-200px',
+
+    });
+    sr.reveal('.title',{
+        duration: 3000,
+        origin: 'left',
+        distance:'-200px',
+
+    });
+    sr.reveal('.about__item',{
+        duration: 4000,
+        origin: 'top',
+        distance:'-200px',
+
+    });
+    sr.reveal('.productive__texts_conteiner',{
+        duration: 3000,
+        origin: 'left',
+        distance:'-200px',
+
+    });
+
+
+    sr.reveal('.title_signup',{
+        duration: 4000,
+        origin: 'left',
+        distance:'-200px',
+
+    });
+    
+  
+    sr.reveal('.form_title',{
+        duration: 3300,
+        origin: 'left',
+        distance:'-200px',
+
+    }); sr.reveal('.form_group',{
+        duration: 3500,
+        origin: 'left',
+        distance:'-200px',
+
+    }); sr.reveal('.form_submit',{
+        duration: 3700,
+        origin: 'left',
+        distance:'-200px',
+
+    }); sr.reveal('.form_paragraph',{
+        duration: 4000,
+        origin: 'left',
+        distance:'-200px',
+
+    });
+    sr.reveal('.title',{
+        duration: 4000,
+        origin: 'rigth',
+        distance:'-400px',
+
+    });
+    sr.reveal('.routing',{
+        duration: 4000,
+        origin: 'bottom',
+        distance:'-400px',
+
+    }); sr.reveal('.cards',{
+        duration: 4000,
+        origin: 'rigth',
+        distance:'-400px',
+
+    }); sr.reveal('.cards2',{
+        duration: 4000,
+        origin: 'left',
+        distance:'-400px',
+
+    });
+  
